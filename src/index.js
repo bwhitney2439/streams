801965c1e31dd4c5878ab9508d7b6c92f3f4ab9b
Whitney <benjamin.whitney@linklaters.com>
2019-07-22 15:45:42 +0100

    cxax

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { provider, connect } from "react-redux";
+import { createStore } from "redux";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Providor store={store}>
+    <App />
+  </Providor>,
+  document.querySelector("#root")
+);
