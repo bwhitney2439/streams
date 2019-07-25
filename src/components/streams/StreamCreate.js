a2e3d17f44c849c2accb066e908c791cbcadb312
DESKTOP-OMKAQP9\bwhitney <ben.whitn@gmail.com>
2019-07-25 09:43:52 +0100

    asda

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 import { Field, reduxForm } from "redux-form";
 
@@ -24,9 +26,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -59,7 +61,14 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  {
+    createStream
+  }
+)(formWrapped);
